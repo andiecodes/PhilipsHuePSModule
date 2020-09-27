ea6536938557862e3923ee8db5aafdbc0cb68a46
andiecodes <morganh@gmx.li>
2020-09-27 22:42:56 +0200

    done

--- phpsm.psm1
+++ phpsm.psm1
@@ -2,16 +2,15 @@
 Author: PS_Nerd
 Purpose: Module to Connect to Philips Hue Lights via Powershell
 #>
-
 #cls
 
 #region IMPORT cmdlets
-get-childitem
-
+$AllPSFiles = get-childitem -filter *.ps1 | Select-Object FullName
+foreach($File in $AllPSFiles){Import-Module -Name $File.FullName}
 #endregion IMPORT cmdlets
 
-
 #region Main
+
 
 #$PhilipsHueBridge = get-PHBridge -StoreConfig
 
